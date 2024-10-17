8f38969acd18fac3ca13b1bcaef62f145e6ebb6f
0xNcat <daniel.juarez@iteso.mx>
2024-10-17 15:52:48 -0600

    added support to N disks

--- Hanoi.asm
+++ Hanoi.asm
@@ -1,27 +1,20 @@
+# =======HANOI TOWERS ON RISC-V ASSEMBLY=======
+# AUTHOR: Daniel Alejandro Juarez Mota
+# =============================================
+
+
 .text
-	lui s0, 0x10010 # FIRST DATA SEGMENT MEMORY ADDRESS POINTER (USED TO ACCOMODATE VALUES ON TOWERS)
+	addi s0, zero, 0xA # N
+	lui s1, 0x10010 # FIRST DATA SEGMENT MEMORY ADDRESS POINTER (USED TO ACCOMODATE VALUES ON TOWERS)	
 	
-	# TOWER A -> 0x10010 + 0x0
-	# TOWER B -> 0x10010 + 0x4
-	# TOWER C -> 0x10010 + 0x8
-	
-	addi s1, zero, 0x3 # N
-	addi a2, zero, 0x1 # DISC 1
-	addi a3, zero, 0x2 # DISC 2
-	addi a4, zero, 0x3 # DISC 3
-	
-	# LOAD DISCS ON THEIR INITIAL POSITION (TOWER A)
-	sw a2, 0x0(s0) 
-	sw a3, 0x20(s0)
-	sw a4, 0x40(s0)
-	
-	
+LOOP:	blt t0, s0, TRUE
 	jal towerOfHanoi
+	
+TRUE:
+	addi t0, t0, 0x1
+	sw t0, 0x0(s1)
+	addi s1, s1, 0x20
+	jal LOOP
 	
 	
 towerOfHanoi:
-	addi t0, zero, 1 # INITIALIZATION OF INTEGER VALUE TO COMPARE WITH N
-	beq s1, t0, TRUE # "if (n == 1)"
-	 
-	
-TRUE: nop
